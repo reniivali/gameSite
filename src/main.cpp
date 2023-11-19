a06576d23f36c6e3288961f5de329b4df98b5a0f
Rhi <renii@reniivali.com>
2023-11-18 17:22:28 -0700

    platform implementation

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@ player ply = {
 	/*xVel, yVel*/0, 0,
 	/*xCap*/      10.0f,
 	/*friction*/  0.9f,
-	/*jumpHeight*/15.0f,
+	/*jumpHeight*/12.5f,
 	/*movSpeed*/  2.5f,
 	/*airFactor*/ 0.5f,
 	/*coins*/     0,
@@ -44,17 +44,11 @@ player ply = {
 	/*grounded*/  false
 };
 
-struct predict {
+struct obstacle {
 	float x, y;
-	float vx, vy;
-};
-
-struct predictions {
-	predict pr[10];
-};
-
-struct crosshair {
-	float x, y;
+	int w, h;
+	int bord;
+	int type;
 };
 
 struct coord {
@@ -65,16 +59,15 @@ struct rotRect {
 	coord tl, tr, bl, br;
 };
 
-crosshair cross = { S_WIDTH / 2, S_HEIGHT / 2 };
-
-void renderCrosshair() {
-	C2D_DrawRectSolid(cross.x - 10, cross.y - 2, 0, 20, 4, C2D_Color32(0x50, 0x50, 0x50, 0xFF));
-	C2D_DrawRectSolid(cross.x - 2, cross.y - 10, 0, 4, 20, C2D_Color32(0x50, 0x50, 0x50, 0xFF));
-}
-
 bool paused = false;
-bool showPaths = false;
 float gravity = 0.5f;
+
+// platform = 0, wall = 1
+const int worldSize = 2;
+obstacle world[worldSize] = {
+	{250,100,100,20,3,0},
+	{250,120,20,120,3,1}
+};
 
 static void drawGradientRect(float x, float y, float w, float h, float p, u32 color, int r1, int g1, int b1, int r2, int g2, int b2, int opacity) {
 	C2D_DrawRectangle(x, y, 0, w, h, C2D_Color32(r1, g1, b1, opacity), C2D_Color32((r1*w/(w+h) + r2*h/(w+h)), (g1*w/(w+h) + g2*h/(w+h)), (b1*w/(w+h) + b2*h/(w+h)), opacity), C2D_Color32((r1*h/(w+h) + r2*w/(w+h)), (g1*h/(w+h) + g2*w/(w+h)), (b1*h/(w+h) + b2*w/(w+h)), opacity), C2D_Color32(r2, g2, b2, opacity));
@@ -187,9 +180,11 @@ int main(int argc, char **argv) {
 		printf("\x1b[1;0HFrame: %i", frame);
 		printf("\x1b[2;0HCPU: %6.2f%% | GPU: %6.2f%%\x1b[K", C3D_GetProcessingTime()*6.0f, C3D_GetDrawingTime()*6.0f);
 		printf("\x1b[3;0HCmdBuf:  %6.2f%%\x1b[K", C3D_GetCmdBufUsage()*100.0f);
-		printf("\x1b[4;0HGravity: %f", gravity);
-		printf("\x1b[5;0HFriction: %f", ply.friction);
-		if (paused) printf("\x1b[7;0HPHYSICS PAUSED"); else printf("\x1b[7;0H              ");
+		printf("\x1b[4;0HPlayer X: %f", ply.x);
+		printf("\x1b[5;0HPlayer XVel: %f", ply.xVel);
+		printf("\x1b[7;0HPlayer Y: %f", ply.y);
+		printf("\x1b[8;0HPlayer YVel: %f", ply.yVel);
+		if (paused) printf("\x1b[15;0HPHYSICS PAUSED"); else printf("\x1b[15;0H              ");
 
 		if (kDown & KEY_DLEFT) ply.mov.l = true;
 		if (kDown & KEY_DRIGHT) ply.mov.r = true;
@@ -201,8 +196,6 @@ int main(int argc, char **argv) {
 			ply.yVel -= ply.jumpHeight;
 			ply.grounded = false;
 		}
-
-		if (kDown & KEY_L) showPaths = !showPaths;
 
 		// calculate physics
 		if (!paused) {
@@ -225,6 +218,42 @@ int main(int argc, char **argv) {
 			ply.x += ply.xVel;
 			ply.y += ply.yVel;
 
+			//obstacle collision
+			bool onPlatform = false;
+			for (int i = 0; i < worldSize; i++) {
+				if (ply.x + ply.w >= world[i].x && ply.x <= world[i].x + world[i].w && ply.y + ply.h >= world[i].y && ply.y <= world[i].y + world[i].h) {
+					switch (world[i].type) {
+						case 0:
+							if (ply.y >= world[i].y) {
+								// colliding with bottom
+								ply.y = world[i].y + world[i].h;
+								ply.yVel = 0;
+							} else if (ply.y <= world[i].y) {
+								//colliding with top
+								ply.y = world[i].y - ply.h;
+								ply.yVel = 0;
+								ply.grounded = true;
+								onPlatform = true;
+							}
+							break;
+						case 1:
+							if (ply.x >= world[i].x) {
+								//colliding with right
+								ply.x = world[i].x + world[i].w;
+								ply.xVel = 0;
+							} else if (ply.x <= world[i].x) {
+								//colliding with left
+								ply.x = world[i].x - ply.w;
+								ply.xVel = 0;
+							}
+							break;
+					}
+				} else if (!onPlatform) ply.grounded = false;
+			}
+
+			
+
+			//bounds collision, keep at bottom
 			if (ply.y + ply.h >= S_HEIGHT) {
 				ply.y = S_HEIGHT - ply.h;
 				ply.yVel = 0;
@@ -248,6 +277,11 @@ int main(int argc, char **argv) {
 		C3D_FrameBegin(C3D_FRAME_SYNCDRAW);
 		C2D_TargetClear(top, clrClear);
 		C2D_SceneBegin(top);
+
+		// draw world
+		for (int i = 0; i < worldSize; i++) {
+			drawGradientRect(world[i].x, world[i].y, world[i].w, world[i].h, 3, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF), 0x6C, 0x70, 0x86, 0x6C, 0x70, 0x86, 255);
+		}
 
 		// draw player
 		drawGradientRect(ply.x, ply.y, ply.w, ply.h, 3, C2D_Color32(0xF5, 0xC2, 0xE7, 0xFF), 0x6C, 0x70, 0x86, 0x6C, 0x70, 0x86, 255);
