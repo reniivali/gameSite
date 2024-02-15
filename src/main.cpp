03dc8a14b3ec2eea539caf6f729c3b454fab8fdc
Rhi <renii@reniivali.com>
2024-02-15 09:39:48 -0700

    colors can now be changed for each individual object

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ struct player {
 };
 
 player ply = {
-	/*X, Y*/      20, 9750,
+	/*X, Y*/      20, 9950,
 	/*W, H*/      20, 40,
 	/*xVel, yVel*/0, 0,
 	/*xCap*/      10.0f,
@@ -54,13 +54,14 @@ struct obstacle {
 	int bord;
 	int type;
 	int d1, d2;
+	u32 col;
 };
 
-// platform = 0, wall = 1, coin = 2, portal = 3, jumpPad = 4, deco = 5, decotri = 6, decotribord = 7;
+// platform = 0, wall = 1, coin = 2, portal = 3, jumpPad = 4, deco = 5, decotri = 6;
 // d1 and d2 are extra properties for certain object types
 // tris are x1, y1, x2, y2, opacity, type, x3, y3
 bool disableDecor = false;
-const int worldSize = 40;
+const int worldSize = 46;
 const int worldHeight = 10000;
 const int worldWidth = 10000;
 const int gridSize = 50;
@@ -70,39 +71,39 @@ int screenPosY = 750;
 // 0 = 340 x
 // 0 = 9820 y
 obstacle lamp[6] = {
-	{60 , 6, 10 , 164 , 5   , 5, udef, udef}, //pole
-	{50 , 0, 30 , 6   , 3   , 5, udef, udef}, //bulb
-	{50 , 6, 50 , 170 , 0x50, 7, 0   , 170 }, //out-left
-	{80 , 6, 80 , 170 , 0x50, 7, 130 , 170 }, //out-right
-	{50 , 6, 50 , 170 , 0x50, 7, 80  , 6   }, //inner-left
-	{80 , 6, 80 , 170 , 0x50, 7, 50  , 170 }  //inner-right
+	{60 , 6, 10 , 164 , 5   , 5, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)}, //pole
+	{50 , 0, 30 , 6   , 3   , 5, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)}, //bulb
+	{50 , 6, 50 , 170 , udef, 6, 0   , 170 , C2D_Color32(0xFF, 0xF9, 0xD8, 0x50)}, //out-left
+	{80 , 6, 80 , 170 , udef, 6, 130 , 170 , C2D_Color32(0xFF, 0xF9, 0xD8, 0x50)}, //out-right
+	{50 , 6, 50 , 170 , udef, 6, 80  , 6   , C2D_Color32(0xFF, 0xF9, 0xD8, 0x50)}, //inner-left
+	{80 , 6, 80 , 170 , udef, 6, 50  , 170 , C2D_Color32(0xFF, 0xF9, 0xD8, 0x50)}  //inner-right
 };
 
 obstacle transLamp(int x, int y, int i) {
 	if (i == 0 || i == 1) {
-		return {lamp[i].x + x, lamp[i].y + y, lamp[i].w    , lamp[i].h, lamp[i].bord    , lamp[i].type, lamp[i].d1    , lamp[i].d2    };
+		return {lamp[i].x + x, lamp[i].y + y, lamp[i].w    , lamp[i].h, lamp[i].bord    , lamp[i].type, lamp[i].d1    , lamp[i].d2    , lamp[i].col};
 	} else {
-		return {lamp[i].x + x, lamp[i].y + y, lamp[i].w + x, lamp[i].h + y, lamp[i].bord, lamp[i].type, lamp[i].d1 + x, lamp[i].d2 + y};
+		return {lamp[i].x + x, lamp[i].y + y, lamp[i].w + x, lamp[i].h + y, lamp[i].bord, lamp[i].type, lamp[i].d1 + x, lamp[i].d2 + y, lamp[i].col};
 	}
 }
 
 obstacle world[worldSize] = {
 	//walls
-	{-5           , 0             , 15           , worldHeight, 3, 1, udef, udef}, //left border
-	{10           , -5            , worldWidth-20, 15         , 3, 0, udef, udef}, //top border
-	{worldWidth-10, 0             , 15           , worldHeight, 3, 1, udef, udef}, //right border
-	{10           , worldHeight-10, worldWidth-20, 15         , 3, 0, udef, udef}, //bottom border
-	{0            , worldHeight-7 , 15           , 7          , 0, 5, udef, udef}, //bottom-left cover
-	{worldWidth-15, worldHeight-7 , 15           , 7          , 0, 5, udef, udef}, //bottom-right cover
-	{0            , 0             , 15           , 7          , 0, 5, udef, udef}, //top-left cover
-	{worldWidth-15, 0             , 15           , 7          , 0, 5, udef, udef}, //top-right cover
+	{-5           , 0             , 15           , worldHeight, 3, 1, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)}, //left border
+	{10           , -5            , worldWidth-20, 15         , 3, 0, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)}, //top border
+	{worldWidth-10, 0             , 15           , worldHeight, 3, 1, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)}, //right border
+	{10           , worldHeight-10, worldWidth-20, 15         , 3, 0, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)}, //bottom border
+	{0            , worldHeight-7 , 15           , 7          , 0, 5, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)}, //bottom-left cover
+	{worldWidth-15, worldHeight-7 , 15           , 7          , 0, 5, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)}, //bottom-right cover
+	{0            , 0             , 15           , 7          , 0, 5, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)}, //top-left cover
+	{worldWidth-15, 0             , 15           , 7          , 0, 5, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)}, //top-right cover
 	//objects
-	{250, 9840, 100, 20  , 3   , 0, udef, udef},
-	{250, 9860, 20 , 130 , 3   , 1, udef, udef},
-	{290, 9880, 20 , 20  , 3   , 2, udef, udef},
-	{270, 9860, 270, 9990, 0x50, 7, 350 , 9860},
-	{500, 9980, 20 , 10  , 3   , 4, 15  , udef},
-	{800, 9800, 80 , 80  , 3   , 3, 10  , 10  },
+	{250, 9840, 100, 20  , 3   , 0, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)},
+	{250, 9860, 20 , 130 , 3   , 1, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)},
+	{290, 9880, 20 , 20  , 3   , 2, udef, udef, C2D_Color32(0xF5, 0xC2, 0xE7, 0xFF)},
+	{270, 9860, 270, 9990, 0x50, 6, 350 , 9860, C2D_Color32(0x6C, 0x70, 0x86, 0x50)},
+	{500, 9980, 20 , 10  , 3   , 4, 15  , udef, C2D_Color32(0xA6, 0xE3, 0xA1, 0xFF)},
+	{800, 9800, 80 , 80  , 3   , 3, 10  , 10  , C2D_Color32(0xCB, 0xA6, 0xF7, 0xFF)},
 
 	//lamp 1
 	transLamp(340, 9820, 0), transLamp(340, 9820, 1), transLamp(340, 9820, 2),
@@ -113,20 +114,23 @@ obstacle world[worldSize] = {
 	transLamp(500, 9820, 3), transLamp(500, 9820, 4), transLamp(500, 9820, 5),
 
 	//top-left
-	{10 , 100, 100, 20 , 3   , 0, udef, udef},
-	{90 , 120, 20 , 50 , 3   , 1, udef, udef},
-	{90 , 170, 100, 20 , 3   , 0, udef, udef},
-	{7  , 103, 6  , 14 , 0   , 5, udef, udef},
-	{93 , 103, 14 , 70 , 0   , 5, udef, udef},
-	{10 , 300, 300, 20 , 3   , 0, udef, udef},
-	{290, 280, 200, 20 , 3   , 0, udef, udef},
-	{200, 360, 400, 20 , 3   , 0, udef, udef},
-	{310, 300, 330, 300, 0xFF, 7, 310 , 320 },
-	{307, 294, 330, 294, 0xFF, 6, 307 , 317 },
-	{270, 300, 290, 300, 0xFF, 7, 290 , 280 },
-	{273, 303, 293, 303, 0xFF, 6, 293 , 283 },
-	{293, 297, 20 , 6  , 0   , 5, udef, udef},
-	{7  , 303, 6  , 14 , 0   , 5, udef, udef}
+	{10 , 100, 100, 20 , 3   , 0, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)},
+	{90 , 120, 20 , 50 , 3   , 1, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)},
+	{90 , 170, 100, 20 , 3   , 0, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)},
+	{7  , 103, 6  , 14 , 0   , 5, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)},
+	{93 , 103, 14 , 70 , 0   , 5, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)},
+	{10 , 300, 300, 20 , 3   , 0, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)},
+	{290, 280, 200, 20 , 3   , 0, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)},
+	{200, 400, 800, 20 , 3   , 0, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)},
+		//lamp 3
+		transLamp(550, 230, 0), transLamp(550, 230, 1), transLamp(550, 230, 2),
+		transLamp(550, 230, 3), transLamp(550, 230, 4), transLamp(550, 230, 5),
+	{310, 300, 330, 300, 0xFF, 6, 310 , 320 , C2D_Color32(0x6C, 0x70, 0x86, 0xFF)}, //border
+	{307, 294, 330, 294, 0xFF, 6, 307 , 317 , C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)}, //inner
+	{270, 300, 290, 300, 0xFF, 6, 290 , 280 , C2D_Color32(0x6C, 0x70, 0x86, 0xFF)}, //border
+	{273, 303, 293, 303, 0xFF, 6, 293 , 283 , C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)}, //inner
+	{293, 297, 20 , 6  , 0   , 5, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)},
+	{7  , 303, 6  , 14 , 0   , 5, udef, udef, C2D_Color32(0xFA, 0xB3, 0x87, 0xFF)}
 };
 
 static void drawGradientRect(float x, float y, float w, float h, float p, u32 color, int r1, int g1, int b1, int r2, int g2, int b2, int opacity) {
@@ -401,7 +405,7 @@ int main(int argc, char **argv) {
 							world[i].w,
 							world[i].h,
 							world[i].bord,
-							C2D_Color32(0xFA, 0xB3, 0x87, 0xFF),
+							world[i].col,
 							0x6C, 0x70, 0x86,
 							0x6C, 0x70, 0x86,
 							255
@@ -416,7 +420,7 @@ int main(int argc, char **argv) {
 							world[i].w,
 							world[i].h,
 							world[i].bord,
-							C2D_Color32(0xFA, 0xB3, 0x87, 0xFF),
+							world[i].col,
 							0x6C, 0x70, 0x86,
 							0x6C, 0x70, 0x86,
 							255
@@ -430,7 +434,7 @@ int main(int argc, char **argv) {
 							world[i].w,
 							world[i].h,
 							world[i].bord,
-							C2D_Color32(0xF5, 0xC2, 0xE7, 0xFF),
+							world[i].col,
 							0x6C, 0x70, 0x86,
 							0x6C, 0x70, 0x86,
 							255
@@ -444,7 +448,7 @@ int main(int argc, char **argv) {
 							world[i].w,
 							world[i].h,
 							world[i].bord,
-							C2D_Color32(0xCB, 0xA6, 0xF7, 0xFF),
+							world[i].col,
 							0x6C, 0x70, 0x86,
 							0x6C, 0x70, 0x86,
 							255
@@ -458,7 +462,7 @@ int main(int argc, char **argv) {
 							world[i].w,
 							world[i].h,
 							world[i].bord,
-							C2D_Color32(0xA6, 0xE3, 0xA1, 0xFF),
+							world[i].col,
 							0x6C, 0x70, 0x86,
 							0x6C, 0x70, 0x86,
 							255
@@ -472,7 +476,7 @@ int main(int argc, char **argv) {
 							world[i].w,
 							world[i].h,
 							world[i].bord,
-							C2D_Color32(0xFA, 0xB3, 0x87, 0xFF),
+							world[i].col,
 							0x6C, 0x70, 0x86,
 							0x6C, 0x70, 0x86,
 							255
@@ -487,28 +491,12 @@ int main(int argc, char **argv) {
 							) && !disableDecor
 						) {
 							C2D_DrawTriangle(
-								world[i].x  - screenPosX, world[i].y  - screenPosY, C2D_Color32(0xFA, 0xB3, 0x87, world[i].bord),
-								world[i].w  - screenPosX, world[i].h  - screenPosY, C2D_Color32(0xFA, 0xB3, 0x87, world[i].bord),
-								world[i].d1 - screenPosX, world[i].d2 - screenPosY, C2D_Color32(0xFA, 0xB3, 0x87, world[i].bord),
+								world[i].x  - screenPosX, world[i].y  - screenPosY, world[i].col,
+								world[i].w  - screenPosX, world[i].h  - screenPosY, world[i].col,
+								world[i].d1 - screenPosX, world[i].d2 - screenPosY, world[i].col,
 								0
 							);
 						} else drawn--; //janky ass ahh motherfucker
-						break;
-					case 7:
-						if (
-							(
-								/*X1, Y1*/(world[i].x  >= screenPosX && world[i].x  <= screenPosX + S_WIDTH && world[i].y  >= screenPosY && world[i].y  <= screenPosY + S_HEIGHT) ||
-								/*X2, Y2*/(world[i].w  >= screenPosX && world[i].w  <= screenPosX + S_WIDTH && world[i].h  >= screenPosY && world[i].h  <= screenPosY + S_HEIGHT) ||
-								/*X3, Y3*/(world[i].d1 >= screenPosX && world[i].d1 <= screenPosX + S_WIDTH && world[i].d2 >= screenPosY && world[i].d2 <= screenPosY + S_HEIGHT)
-							) && !disableDecor
-						) {
-							C2D_DrawTriangle(
-								world[i].x  - screenPosX, world[i].y  - screenPosY, C2D_Color32(0x6C, 0x70, 0x86, world[i].bord),
-								world[i].w  - screenPosX, world[i].h  - screenPosY, C2D_Color32(0x6C, 0x70, 0x86, world[i].bord),
-								world[i].d1 - screenPosX, world[i].d2 - screenPosY, C2D_Color32(0x6C, 0x70, 0x86, world[i].bord),
-								0
-							);
-						} else drawn--;
 						break;
 				}
 				drawn++;
